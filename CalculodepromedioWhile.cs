8b66b22a612f2ef9dc25756938a53d097d27d544
Daniela Ochoa <danielaochoa_o@hotmail.com>
2020-02-11 08:34:54 -0500

    Update CalculodepromedioWhile.cs

--- CalculodepromedioWhile.cs
+++ CalculodepromedioWhile.cs
@@ -10,20 +10,20 @@ namespace CalculodepromedioWhile
     {
         static void Main(string[] args)
         {
-            double total = 0;
-            string respuesta = "s";
+            double total = 0; //Total de la sumatoria
+            string respuesta = "s";//Primera vez
 
-            int i = 0;
+            int i = 0; //Contador de cuántas veces entran en el ciclo while
 
             while (respuesta == "s"){ //Mientras sea si
 
                 Console.WriteLine("Edad: ");
                 int edad = int.Parse(Console.ReadLine()); //Ingrese edad
-                total += edad; 
+                total += edad; //Acumular cada edad nueva que entra
 
                 Console.WriteLine("¿Existen más datos? (s/n)");
                 respuesta = Console.ReadLine();
-                i++;
+                i++;//Se le suma +1 al contador
             }
 
             double promedio = total / i;
