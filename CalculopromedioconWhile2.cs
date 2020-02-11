0e971dfb8021b0d2af880b204ed7f7e3b5e74f19
Daniela Ochoa <danielaochoa_o@hotmail.com>
2020-02-11 08:40:13 -0500

    Update CalculopromedioconWhile2.cs

--- CalculopromedioconWhile2.cs
+++ CalculopromedioconWhile2.cs
@@ -11,17 +11,17 @@ namespace CalculopromedioconWhile2
         static void Main(string[] args)
         {
             double total = 0; //Resultado de la sumatoria
-            int i = 0; //Contador
+            int i = 0; //Contador (cuantos datos son)
 
             Console.WriteLine("Ingrese el número de datos (n): ");
-            int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine()); //Límite de cantidad de datos (n)
 
-            while (i < n) { //Mientras n sea mayor que i
+            while (i < n) { //Mientras n sea mayor que contador
 
                 Console.WriteLine("Edad: ");
                 int edad = int.Parse(Console.ReadLine()); //Ingrese edad
                 total += edad;
-                i++;
+                i++; //++ es igual a decir +=1
             }
 
             double promedio = total / n;
