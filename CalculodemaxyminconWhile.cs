15cf3e02e9a0f9405b77cfc31196e3fddb47fbb5
Daniela Ochoa <danielaochoa_o@hotmail.com>
2020-02-11 08:47:00 -0500

    Update CalculodemaxyminconWhile.cs

--- CalculodemaxyminconWhile.cs
+++ CalculodemaxyminconWhile.cs
@@ -11,18 +11,18 @@ namespace CalculodemaxyminconWhile
         static void Main(string[] args)
         {
             double total = 0; //Resultado de la sumatoria
-            int i = 0, max = 0, min = 200;
+            int i = 0, max = 0, min = 200; //minimo debe ser más alto para comparar con los datos que entren
 
             Console.WriteLine("Ingrese el número de datos (n): ");
-            int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());//Limite de datos
 
             while (i < n) { //Mientras n sea mayor que i
 
                 Console.WriteLine("Edad: ");
                 int edad = int.Parse(Console.ReadLine()); //Ingrese edad
 
-                if (edad > max) max = edad;
-                if (edad < min) min = edad;
+                if (edad > max) max = edad; //Encontré un número mucho más alto
+                if (edad < min) min = edad; //Encontré un número incluso más bajo
 
                 total += edad;
                 i++;
