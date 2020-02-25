e34f3309466ba914997fc90d56d36b51744853d4
Daniela Ochoa <danielaochoa_o@hotmail.com>
2020-02-25 09:35:03 -0500

    EjercicioSenoForDiapo11Clase

---
+++ EjercicioSenoForDiapo11Clase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSenoForDiapo11Clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = 0.5;
+            double seno = 0;
+
+            
+            for (int i = 0; i < 100; i++)
+            {
+
+                double faci = Factorial(i);
+
+                seno += ((Math.Pow(-1, i)) / (Factorial(2 * i + 1)) * Math.Pow(x, 2 *i + 1));
+                Console.WriteLine("Seno: " + seno);
+
+            }
+        }
+
+        static double Factorial(double smfactorial){
+
+            if (smfactorial <= 1) return 1;
+            return smfactorial * Factorial(smfactorial - 1);
+
+        }
+
+    }
+}
