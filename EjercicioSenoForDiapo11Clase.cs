9d1a1be707c289a83489998ad07854148ee9f678
Daniela Ochoa <danielaochoa_o@hotmail.com>
2020-02-26 11:34:27 -0500

    Update EjercicioSenoForDiapo11Clase.cs

--- EjercicioSenoForDiapo11Clase.cs
+++ EjercicioSenoForDiapo11Clase.cs
@@ -17,19 +17,21 @@ namespace EjercicioSenoForDiapo11Clase
             
             for (int i = 0; i < 100; i++)
             {
-
+                //Nombrar variable Factorial
                 double faci = Factorial(i);
-
+                //fórmula seno
                 seno += ((Math.Pow(-1, i)) / (Factorial(2 * i + 1)) * Math.Pow(x, 2 *i + 1));
+                
+                //Salida seno
                 Console.WriteLine("Seno: " + seno);
 
             }
         }
+                //Creación de nuestra propia fórmula para el factorial
+                static double Factorial(double smfactorial){
 
-        static double Factorial(double smfactorial){
-
-            if (smfactorial <= 1) return 1;
-            return smfactorial * Factorial(smfactorial - 1);
+                         if (smfactorial <= 1) return 1;
+                         return smfactorial * Factorial(smfactorial - 1);
 
         }
 
