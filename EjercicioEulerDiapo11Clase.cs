050bc15c4e897341a399c01c07a4c4f919338494
Daniela Ochoa <danielaochoa_o@hotmail.com>
2020-02-26 11:21:31 -0500

    Update EjercicioEulerDiapo11Clase.cs

--- EjercicioEulerDiapo11Clase.cs
+++ EjercicioEulerDiapo11Clase.cs
@@ -10,24 +10,30 @@ namespace EjercicioEulerDiapo11Clase
     {
         static void Main(string[] args)
         {
+            //entradas
             int x = 3;
             double euler= 0;
 
+            
             for (int i = 0; i < 100; i++)
             {
+                //Nombre de la variable factorial
                 double faci = Factorial(i);
                 
+                //fórmula de euler
                 euler += (Math.Pow(x, i) / Factorial(i));
+                
+                //Salida valor de euler
                 Console.WriteLine("Euler: " + euler);
                 
             }
         
-        }
+        }   //Creación de nuestra propia fórmula para el factorial
 
-        static double Factorial(double smfactorial) {
-
-            if (smfactorial <= 1) return 1;
-            return smfactorial * Factorial(smfactorial- 1);
+            static double Factorial(double smfactorial) {
+                
+                if (smfactorial <= 1) return 1;
+                return smfactorial * Factorial(smfactorial- 1);
 
         }
     }
