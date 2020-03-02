8ad9231af54738988175f79a1c1ac160dfc00b5c
Daniela Ochoa <danielaochoa_o@hotmail.com>
2020-03-02 14:27:10 -0500

    IntentoOrden

---
+++ IntentodeOrdenamiento.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioBlackJackConNombresyArreglosDiapo13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random aleatorio = new Random();
+
+            //Variables
+            string mascarta = "";
+
+            //Inicio programa
+            Console.WriteLine("¡¡¡¡¡¡¡¡¡¡¡¡¡¡Incio!!!!!!!!!!!!!");
+
+
+            Console.WriteLine("Desea seguir(s/n): ");
+            string respuesta = Console.ReadLine();
+
+
+            Console.WriteLine("\nIngrese el número de jugadores");
+            int n = int.Parse(Console.ReadLine());
+
+            //Arreglo puntajes
+
+            int[] puntajes = new int[n];
+
+            //string[] nombres = new string[n];
+
+            for (int i = 0; i < puntajes.Length; i++){
+
+
+               /* for (int j = 0; j < nombres.Length; j++)
+                {
+
+
+                    Console.WriteLine("\nIngrese el nombre del jugador");
+                }*/
+
+
+                    Console.WriteLine("\n-----------------Bienvenido al juego----------------- ");
+                    Console.WriteLine("\n                 (Jugador " + (i + 1) + " Nombre: " +  ")");
+
+
+                    int carta1 = aleatorio.Next(1, 11);
+                    int carta2 = aleatorio.Next(1, 11);
+
+                    //Valores primeras cartas (salida)
+                    Console.WriteLine("\nSu primera carta fue: " + carta1);
+                    Console.WriteLine("Su segunda carta fue: " + carta2);
+
+                    //Varialble nueva carta y puntaje
+                    int ncarta;
+                    int total = carta1 + carta2;
+
+                    Console.WriteLine("\nTotal: " + total); //Salida total
+
+                    //Mientras el total sea menor que 21
+                    while (total < 21)
+                    {
+
+                        Console.Write("\n¿Desea una nueva carta? (s/n): ");
+                        mascarta = Console.ReadLine();
+                        if (mascarta == "n")
+                        {
+                            break;
+                        }
+
+                        ncarta = aleatorio.Next(1, 11); //Nueva carta aleatoria
+                        total += ncarta;
+
+                        Console.WriteLine("\nNueva carta: " + ncarta); //Salida nueva carta
+                        Console.WriteLine("Total: " + total); //Salida total
+
+                        //Condición de ganar
+                        if (total == 21)
+                        {
+
+                            Console.WriteLine("\n        ¡GANASTE!");
+                            break;
+                        }
+
+                        //Condición de perder
+                        if (total > 21)
+                        {
+
+                            Console.WriteLine("\n     ¡Te pasaste de 21!");
+                            Console.WriteLine("        Fin del juego");
+                            total = 0;
+                            Console.WriteLine("Total: " + total);
+                            break;
+                        }
+                    }
+                    puntajes[i] = total;
+                
+
+            }
+
+            //Variables máx y min
+            int max = 0;
+            int indmax = 0;
+
+            //Arreglo puntaje máximo
+            for (int i = 0; i < puntajes.Length; i++)
+            {
+                if (puntajes[i] > max)
+                {
+
+                    max = puntajes[i];
+                    indmax = i;
+                }
+            }
+
+            Console.WriteLine("\n              #1 Puesto. Jugador # " + (indmax + 1) + " con un puntaje de: " + puntajes[indmax]);
+
+            //Ordenamiento puntajes
+
+            for (int k = 0; k < puntajes.Length - 1; k++) {
+
+                for (int i = 0; i < puntajes.Length -1; i++) {
+                    if (puntajes[i] < puntajes [i+1]) {
+
+
+                        int tmp = puntajes[i];
+                        puntajes[i] = puntajes[i + 1];
+                        puntajes[i + 1] = tmp;
+                       
+                    }
+                }
+            }
+
+            //Primeros 3 lugares, puntajes y nombres
+
+            Console.WriteLine("Los mejores puntajes son: ");
+            int longitud = Math.Min(n, puntajes.Length);
+            for (int i = 0; i < puntajes.Length - 1; i++)
+            {
+                Console.WriteLine("Puntaje: " + puntajes[i]);
+
+            }
+
+        }
+    }
+}
